9032839442614c8fc8efefc1c3915eb75cfe184d
sahidAle <sahidalejandroo@gmail.com>
2023-06-19 12:35:19 -0600

    Modulo movies

---
+++ movies/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Modulo de movies REPO',
+    'version': '1.0',
+    'depends': [
+
+    ],
+    'author': 'Sahid Alejandro',
+    'category':  'Movies',
+    'website': 'http://www.google.com',
+    'summary': 'Modulo de presupuestos para peliculas',
+    'description': 'Modulo para hacer presupuestos de peliculas',
+    'data': [    ],
+
+}
